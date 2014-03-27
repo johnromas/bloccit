1b1779331ea490055d5c6f0a398f7ab930256212
John Romas <john.romas87@gmail.com>
2014-03-27 12:57:33 -0400

    Associated users and posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :comments
+  belongs_to :user
+
+  default_scope { order('created_at DESC') }
 
   # 1. Takes a look at the DB tables ``posts``
   # 2. Looks at the columns and greates getter/setters for each
