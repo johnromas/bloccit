6bbae40c89efc5b3a7b611694d4284074218f458
John Romas <john.romas87@gmail.com>
2014-03-27 22:46:12 -0400

    Associated and nested topics and posts

--- db/migrate/20140327163104_add_user_to_posts.rb
+++ db/migrate/20140327163104_add_user_to_posts.rb
@@ -1,6 +1,7 @@
-class AddUserToPosts < ActiveRecord::Migration
+class a < ActiveRecord::Migration
   def change
     add_column :posts, :user_id, :integer
     add_index :posts, :user_id
   end
 end
+rails g migration AddUserToPosts user_id:integer
